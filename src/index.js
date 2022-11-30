b64fa6db09b8d1ea0cf4496384e2a6c4483c4be3
root <root@localhost.localdomain>
2022-11-30 10:49:34 -0500

    update code

--- src/index.js
+++ src/index.js
@@ -2,22 +2,19 @@ const _ = require('lodash')
 const github = require('@actions/github');
 const core = require('@actions/core');
 const fs = require('fs');
+const { graphql } = require("@octokit/graphql");
 
 async function run() {
     const myToken = core.getInput('token');
     const type = core.getInput('type')
     const repoFull = core.getInput('repo').split('/');    
-    const variables = core.getInput('variables');
-    const delimiter = core.getInput('delimiter');
-    const filePath = core.getInput('filePath');
-    const outputFile = core.getInput('outputFile');
-    const writeToFile = core.getInput('writeToFile');
+    
     
     let semVersion = '1.0.0'
     
 
 
-    fs.appendFileSync(process.env.GITHUB_OUTPUT, "version=" + semVersion);
+    fs.appendFileSync(process.env.GITHUB_OUTPUT, "version=" + core.getInput('repo'));
     const octokit = github.getOctokit(myToken)
     
     
