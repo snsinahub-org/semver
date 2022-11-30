a49e3fdd816f8f4ac394a586d75ee1f729ee7917
root <root@localhost.localdomain>
2022-11-30 11:02:30 -0500

    update code

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ const { graphql } = require("@octokit/graphql");
 async function run() {
     const myToken = core.getInput('token');
     const type = core.getInput('type')
-    const repoFull = core.getInput('repo').split('/');    
+    const repoFull = core.getInput('repo').split('/');   
+    let owner = repoFull[0];
+    let repo = repoFull[1] 
     
     
     let semVersion = '1.0.0'
