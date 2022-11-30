9c42ca5113205cf59faa2d859afd2346f8915bf3
root <root@localhost.localdomain>
2022-11-30 15:46:48 -0500

    update code

--- src/index.js
+++ src/index.js
@@ -7,35 +7,47 @@ const { graphql } = require("@octokit/graphql");
 async function run() {
     const myToken = core.getInput('token');
     const type = core.getInput('type')
-    const repoFull = core.getInput('repo').split('/');   
+    const repoFull = core.getInput('repo').split('/');
     let owner = repoFull[0];
-    let repo = repoFull[1] 
-    
-    
+    let repo = repoFull[1]
+
+
     let semVersion = '1.0.0'
     const graphqlWithAuth = graphql.defaults({
         headers: {
-          authorization: `token ${myToken}`,
+            authorization: `token ${myToken}`,
         },
-      });
+    });
     const { repository } = await graphqlWithAuth(
         `
           {
             repository(owner: "${owner}", name: "${repo}") {
-             
+                refs(refPrefix: "refs/tags/", first: 100, after: $cursor) {
+                    nodes {
+                      name
+                      target {
+                        ... on Tag {
+                          tagger {
+                            date
+                          }
+                        }
+                        
+                      }
+                    }
+                  }
             }
           }
         `
-      );
-    
-      console.log(JSON.stringify(repository));
+    );
+
+    console.log(JSON.stringify(repository));
 
 
     fs.appendFileSync(process.env.GITHUB_OUTPUT, "version=" + repoFull[1]);
     const octokit = github.getOctokit(myToken)
-    
-    
-    
+
+
+
 }
 
 run();
