203c0ac26d36d96e92983ce7a379d01e1d71c8b0
root <root@localhost.localdomain>
2022-12-13 11:23:49 -0500

    update code

--- src/index.js
+++ src/index.js
@@ -22,8 +22,9 @@ async function run() {
     let semVersion = '1.0.0'
     
     const { repository } = await tags.getAllTags(owner, repo, myToken);
-    console.log('REPOSITORY: ', JSON.stringify(repository))
+    
     let tagsObj = tags.getTags(repository);
+    console.log('REPOSITORY: ', JSON.stringify(tagsObj))
     const jsonUtils = new JsonUtils(tagsObj); 
     if(prefix == '') {
         jsonUtils.filterNoPrefix()
