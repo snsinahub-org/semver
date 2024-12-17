17b131aa80b53cc2cf5058521d5af134fa3454db
Siavash Namvar <snsinahub@github.com>
2024-12-16 22:00:20 -0500

    add pagination to the result

--- src/utils/get-tags.js
+++ src/utils/get-tags.js
@@ -30,7 +30,7 @@ module.exports = class GetReleaseTags {
             const response = await graphqlWithAuth(
                 `
                   query($owner: String!, $repo: String!, $endCursor: String) {
-                    repository(owner: ${owner}, name: ${repo}) {
+                    repository(owner: $owner, name: $repo) {
                       releases(first: 100, orderBy: {field: CREATED_AT, direction: DESC}, after: $endCursor) {
                         nodes {
                           name
@@ -46,8 +46,8 @@ module.exports = class GetReleaseTags {
                   }
                 `,
                 {
-                    owner,
-                    repo,
+                    owner: $owner,
+                    repo: $repo,
                     endCursor,
                 }
             );
