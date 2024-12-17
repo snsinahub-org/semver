412abc6524d7fd243a24c0149284900df29d7b7e
Siavash Namvar <snsinahub@github.com>
2024-12-16 21:29:41 -0500

    add pagination to the result

--- src/index.js
+++ src/index.js
@@ -34,6 +34,7 @@ async function run() {
     let repo = repoFull[1]
     
     const { repository } = await tags.getAllTags(owner, repo, myToken);
+    console.log('repository:', JSON.stringify(repository, null, 2));
     
     let tagsObj = tags.getTags(repository);
     let jsonUtils = new JsonUtils(tagsObj); 
